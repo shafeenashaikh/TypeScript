6cda63c511eff7fd9d80ec96e4fd3765db7f99e5
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-20 19:05:15 +0530

    var in typescript example

---
+++ typescript/tutorial.js
@@ -0,0 +1,10 @@
+function greetPerson(name){
+    var greet;
+    if(name === 'jai'){
+         greet ="Hello jai";
+    }else{
+         greet = 'Hi there'
+    }
+    console.log(greet)
+}
+greetPerson("jai")
---
+++ typescript/tutorial.ts
@@ -0,0 +1,9 @@
+function greetPerson(name: any){
+    if(name === 'jai'){
+        var greet ="Hello jai";
+    }else{
+        var greet = 'Hi there'
+    }
+    console.log(greet)
+}
+greetPerson("jai")
