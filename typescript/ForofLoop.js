b84809f4db610fc758333e10469b265eb8114ab4
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-21 19:40:37 +0530

    for of loop

---
+++ typescript/ForofLoop.js
@@ -0,0 +1,8 @@
+var colors = ['Red', 'Blue', 'Green'];
+for (var index in colors) {
+    console.log(colors[index]);
+}
+for (var _i = 0, colors_1 = colors; _i < colors_1.length; _i++) {
+    var color = colors_1[_i];
+    console.log(color);
+}
---
+++ typescript/ForofLoop.ts
@@ -0,0 +1,10 @@
+let colors = ['Red', 'Blue', 'Green'];
+
+for(let index in colors){
+    console.log(colors[index]);
+}
+
+for(let color of colors){
+    console.log(color);
+}
+
