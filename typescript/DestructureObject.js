10677ab6c959e6f06121b8e766e431ee4ddcebe1
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-21 19:22:29 +0530

    Destructuring object

---
+++ typescript/DestructureObject.js
@@ -0,0 +1,9 @@
+var employee = {
+    fname: "Sam",
+    lname: "Bing",
+    gender: "Male"
+};
+var f = employee.fname, l = employee.lname, g = employee.gender;
+console.log(f);
+console.log(l);
+console.log(g);
---
+++ typescript/DestructureObject.ts
@@ -0,0 +1,11 @@
+let employee = {
+    fname: "Sam",
+    lname: "Bing",
+    gender: "Male"
+};
+
+let {fname: f, lname: l, gender: g} = employee;
+
+console.log(f);
+console.log(l);
+console.log(g);
