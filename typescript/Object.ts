c38df9fcc5897b99817f72c4596be8115cd97f67
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-21 19:02:21 +0530

    Object literals in typescript

---
+++ typescript/Object.ts
@@ -0,0 +1,29 @@
+let firstname = "Sam";
+let lastname = 'Bing';
+
+let person = {
+    firstname,
+    lastname
+};
+
+function  createPerson(firstname, lastname, age){
+    let fullname = firstname + " " + lastname;
+    return {
+        firstname, 
+        lastname, 
+        fullname,
+        isSenior(){
+            return age>60;
+        }
+    }
+
+}
+
+
+let p = createPerson("Ross", "Geller", "62");
+console.log(p.firstname);
+console.log(p.lastname);
+console.log(p.fullname);
+console.log(p.isSenior());
+// console.log(person.firstname);
+// console.log(person.lastname);
