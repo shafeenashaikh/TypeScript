6ef7885343eaded2893ad5a11de529fb9c770764
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-21 19:30:53 +0530

    String Template example

---
+++ typescript/String.js
@@ -0,0 +1,3 @@
+var user = "sam";
+var greet = "Welcom  " + user + " to ES2015\n                This is the seconf line.\n                    third and so    on.\n";
+console.log(greet);
---
+++ typescript/String.ts
@@ -0,0 +1,7 @@
+let user = "sam";
+
+let greet = `Welcom  ${user} to ES2015
+                This is the seconf line.
+                    third and so    on.
+`;
+console.log(greet);
