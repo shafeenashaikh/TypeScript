29d1fc7607efc78f71c9b01acf21674ba3e6e83b
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-01 15:01:04 +0530

    Symbol iterator

---
+++ typescript/Symbole1.js
@@ -0,0 +1,8 @@
+var str = "Hello";
+var arr = [1, 2, 3];
+var num = 5;
+var obj = { name: "sam" };
+console.log("For String -" + typeof str[Symbol.iteration]);
+console.log("For String -" + typeof arr[Symbol.iteration]);
+console.log("For String -" + typeof num[Symbol.iteration]);
+console.log("For String -" + typeof obj[Symbol.iteration]);
---
+++ typescript/Symbole1.ts
@@ -0,0 +1,9 @@
+let str = "Hello";
+let arr = [1,2,3];
+let num = 5;
+let obj = {name:"sam"};
+
+console.log("For String -" + typeof str[Symbol.iteration]);
+console.log("For String -" + typeof arr[Symbol.iteration]);
+console.log("For String -" + typeof num[Symbol.iteration]);
+console.log("For String -" + typeof obj[Symbol.iteration]);
