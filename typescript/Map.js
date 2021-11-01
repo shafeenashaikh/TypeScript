ae218f56b0cb672dcf96c3ed836bb84ee42bbd9e
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-01 10:13:02 +0530

    Finding size of map

---
+++ typescript/Map.js
@@ -0,0 +1,10 @@
+var myMap = new Map();
+myMap.set("fname", "sam");
+myMap.set("age", 30);
+console.log(myMap.get("fname"));
+var ob1 = {};
+var ob2 = {};
+myMap.set(ob1, 10);
+mySet.set(ob2, 20);
+console.log(myMap(ob1));
+console.log(myMap.size);
---
+++ typescript/Map.ts
@@ -0,0 +1,14 @@
+let myMap = new Map();
+myMap.set("fname", "sam");
+myMap.set("age", 30);
+
+console.log(myMap.get("fname"));
+
+let ob1 ={};
+let ob2 = {};
+
+myMap.set(ob1, 10);
+mySet.set(ob2, 20);
+
+console.log(myMap(ob1));
+console.log(myMap.size);
